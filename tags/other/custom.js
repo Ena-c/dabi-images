938957c3d0bf6755582571ea3edbe65e9f58bbb3
Ena <anmiciusdeveloper@gmail.com>
2019-06-12 04:13:54 -0500

    Updated custom.js

--- tags/other/custom.js
+++ tags/other/custom.js
@@ -2,7 +2,7 @@ const requestHandler = require("../handler");
 class CustomShit {
     /**
      * @param {string} site Website url
-     * @param {string} regex What to look for in the website contents (Regex)
+     * @param {RegExp} regex What to look for in the website contents (Regex)
      */
     do(site, regex) {
         return requestHandler.makeRequest("other", site, regex);
